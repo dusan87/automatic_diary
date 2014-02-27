98cd56bd25be2194fe9b3a41f828fec7acae1aca
Dusan Ristic <dusan.ristic@quatix.com.br>
2014-02-27 12:53:35 -0300

    Moved everything in fragment

--- Automatic_Diary/src/main/java/com/elfak/automatic_diary/activities/UsersListActivity.java
+++ Automatic_Diary/src/main/java/com/elfak/automatic_diary/activities/UsersListActivity.java
@@ -2,35 +2,15 @@ package com.elfak.automatic_diary.activities;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.FragmentManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.elfak.automatic_diary.R;
-import com.elfak.automatic_diary.adapters.UserAdapter;
-import com.elfak.automatic_diary.api.RestClient;
-import com.elfak.automatic_diary.core.User;
-import com.elfak.automatic_diary.fragments.UsersListFragment;
-import com.google.gson.Gson;
-import com.google.gson.internal.LinkedTreeMap;
-import com.google.gson.reflect.TypeToken;
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-
-import org.json.JSONObject;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by dusanristic on 12/16/13.
  */
 public class UsersListActivity extends Activity{
-
-    RestClient getHttpClient;
-
-    private UserAdapter mAdapter;
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
@@ -38,51 +18,6 @@ public class UsersListActivity extends Activity{
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_users_list);
-
-        mAdapter = new UserAdapter(UsersListActivity.this);
-        RequestParams params = new RequestParams();
-        String username = getIntent().getStringExtra("username");
-        params.put("username", username);
-        // get reference to the Fragment
-
-        getHttpClient = new RestClient();
-        getHttpClient.get("all_users/", params,  new JsonHttpResponseHandler(){
-            @Override
-            public void onSuccess(JSONObject response) {
-                super.onSuccess(response);
-                Gson gson =  new Gson();
-                Map<String,Object> resultFullJsonArrayComplete =  gson.fromJson(response.toString(), new TypeToken<Map<String,Object>>() {}.getType());
-                Toast.makeText(UsersListActivity.this, "SUCCESS", Toast.LENGTH_LONG).show();
-                List<LinkedTreeMap<String,String>> jsonOfResultsComplete = (List<LinkedTreeMap<String,String>>)resultFullJsonArrayComplete.get("results");
-
-                for(LinkedTreeMap<String, String> _user : jsonOfResultsComplete){
-                    User user = new User();
-                    user.setFirstName(_user.get("first_name"));
-                    user.setLastName(_user.get("last_name"));
-                    user.setCurrentCountry(_user.get("country"));
-                    user.setUsername(_user.get("username"));
-                    user.setImage(_user.get("image"));
-                    user.setCurrentCity(_user.get("city"));
-                    user.setBirthDay(_user.get("birth_day"));
-                    user.setGender(_user.get("gender"));
-                    mAdapter.add(user);
-
-                }
-            }
-
-            @Override
-            public void onFailure(int statusCode, Throwable e, JSONObject errorResponse) {
-                super.onFailure(statusCode, e, errorResponse);
-                Toast.makeText(UsersListActivity.this, "ERORRRRR", Toast.LENGTH_LONG).show();
-            }
-
-        });
-
-        FragmentManager fragmentManager = getFragmentManager();
-        UsersListFragment usersListFragment = (UsersListFragment) fragmentManager.findFragmentById(R.id.usersListFragment);
-
-
-        if (usersListFragment != null) usersListFragment.setListAdapter(mAdapter);
 
     }
 
--- Automatic_Diary/src/main/java/com/elfak/automatic_diary/fragments/UsersListFragment.java
+++ Automatic_Diary/src/main/java/com/elfak/automatic_diary/fragments/UsersListFragment.java
@@ -1,6 +1,7 @@
 package com.elfak.automatic_diary.fragments;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.ListFragment;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -14,21 +15,92 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.elfak.automatic_diary.R;
 import com.elfak.automatic_diary.activities.MapActivity;
 import com.elfak.automatic_diary.activities.UserDetailActivity;
+import com.elfak.automatic_diary.adapters.UserAdapter;
+import com.elfak.automatic_diary.api.RestClient;
 import com.elfak.automatic_diary.core.User;
+import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by dusanristic on 12/16/13.
  */
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class UsersListFragment extends ListFragment  {
+
+    private UserAdapter mAdapter;
+
+    RestClient getHttpClient;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mAdapter =  new UserAdapter(getActivity());
+
+        RequestParams params = new RequestParams();
+        String username = getActivity().getIntent().getStringExtra("username");
+        params.put("username", username);
+
+        getHttpClient = new RestClient();
+        getHttpClient.get("all_users/", params,  new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(JSONObject response) {
+                super.onSuccess(response);
+                Gson gson =  new Gson();
+                Map<String,Object> resultFullJsonArrayComplete =  gson.fromJson(response.toString(), new TypeToken<Map<String,Object>>() {}.getType());
+                Toast.makeText(getActivity(), "SUCCESS", Toast.LENGTH_LONG).show();
+                List<LinkedTreeMap<String,String>> jsonOfResultsComplete = (List<LinkedTreeMap<String,String>>)resultFullJsonArrayComplete.get("results");
+
+                for(LinkedTreeMap<String, String> _user : jsonOfResultsComplete){
+                    User user = new User();
+                    user.setFirstName(_user.get("first_name"));
+                    user.setLastName(_user.get("last_name"));
+                    user.setCurrentCountry(_user.get("country"));
+                    user.setUsername(_user.get("username"));
+                    user.setImage(_user.get("image"));
+                    user.setCurrentCity(_user.get("city"));
+                    user.setBirthDay(_user.get("birth_day"));
+                    user.setGender(_user.get("gender"));
+                    mAdapter.add(user);
+
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Throwable e, JSONObject errorResponse) {
+                super.onFailure(statusCode, e, errorResponse);
+                Toast.makeText(getActivity(), "ERORRRRR", Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        setListAdapter(mAdapter);
     }
 
     @Override
