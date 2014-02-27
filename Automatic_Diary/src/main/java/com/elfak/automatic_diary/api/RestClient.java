360189267cfd4b450a46294ff4f94d212a44ff61
Dusan Ristic <dusan.ristic@quatix.com.br>
2014-02-27 12:12:39 -0300

    Rest ip

--- Automatic_Diary/src/main/java/com/elfak/automatic_diary/api/RestClient.java
+++ Automatic_Diary/src/main/java/com/elfak/automatic_diary/api/RestClient.java
@@ -11,7 +11,7 @@ import org.apache.http.client.*;
  */
 public class RestClient {
 
-    private static final String BASE_URL = "http://192.168.1.20:8000/";
+    private static final String BASE_URL = "http://10.0.2.2:8000/";
 
     private static AsyncHttpClient client = new AsyncHttpClient();
 
